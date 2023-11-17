99a93e8071fe2f5176b804ea94bf5f343c648041
Designated-Hitter <dosadola@gmail.com>
2023-11-17 17:40:26 +0900

    feat: implemented page for catchAnswer submit

--- component/footer.tsx
+++ component/footer.tsx
@@ -1,11 +1,43 @@
 "use client";
+import { useState } from "react";
+import Button from '@mui/material/Button';
+import MyModal from "./MyModal";
+
+import { useAtom } from 'jotai';
+import { loginAtom } from "@/app/modules/loginAtoms";
+import CatchAnswer from "@/app/catchAnswer/catchAnswer";
+
+export interface ModalProps {
+    isOpen: boolean,
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>,
+    modalHeader: string,
+    setModalHeader: React.Dispatch<React.SetStateAction<string>>,
+    isLogin: boolean,
+    setIsLogin: React.Dispatch<React.SetStateAction<boolean>>,
+}
+
 export default function Footer() {
+    const [open, setOpen] = useState<ModalProps['isOpen']>(false);
+    const [isLogin,setIsLogin] = useAtom(loginAtom)
+
+    const handleClose = () => { setOpen(false); }
+    const catch_answer = () => { setOpen(true); }
+
     return (<>
         <div className="footerContainer">
             <div>
                 <h4>SWJUNGLE Team.def()</h4>
             </div>
+            {isLogin?
+            <div>
+                {/* 캐치마인드 정답 제출용 모달 호출 버튼 */}
+                <Button onClick={catch_answer}> </Button>
+            </div>
+            :null}
+            
+
         </div>
+        <MyModal open={open} modalHeader={'캐치마인드 정답 입력'} modalContent={<CatchAnswer />} closeFunc={handleClose} />
         <style jsx>{`
                 .footerContainer{
                     display: flex;
