19306b0f3e5f62580d99b71d191b76b1e362a3c2
poqop721 <poqop721@naver.com>
2023-11-17 14:05:04 +0900

    feat:Refactor game pages and add drawing functionality
    Merged QRpage and game page so that if people come in, qr will disappear
    
    #12 gamepage

--- app/catch/page.tsx
+++ app/catch/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useRef, useState, useCallback, useEffect } from 'react';
+import Button from '@mui/material/Button';
 
 interface CanvasProps {
   width: number;
@@ -15,6 +16,9 @@ export default function Catch() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [mousePosition, setMousePosition] = useState<Coordinate | undefined>(undefined);
   const [isPainting, setIsPainting] = useState(false);
+  const [color, setColor] = useState("black");
+  const [lineWidth, setLineWidth] = useState(3);
+  const [isEraser, setIsEraser] = useState(false);
 
   // 좌표 얻는 함수
   const getCoordinates = (event: MouseEvent): Coordinate | undefined => {
@@ -38,9 +42,9 @@ export default function Catch() {
     const context = canvas.getContext('2d');
 
     if (context) {
-      context.strokeStyle = "black";  // 선 색깔
+      context.strokeStyle = isEraser?'white':color;  // 선 색깔
       context.lineJoin = 'round';	// 선 끄트머리(?)
-      context.lineWidth = 5;		// 선 굵기
+      context.lineWidth = lineWidth;		// 선 굵기
 
       context.beginPath();
       context.moveTo(originalMousePosition.x, originalMousePosition.y);
@@ -78,6 +82,18 @@ export default function Catch() {
   const exitPaint = useCallback(() => {
     setIsPainting(false);
   }, []);
+
+  const clearCanvas = () => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const canvas: HTMLCanvasElement = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    if (context) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+  }
   
   useEffect(() => {
     if (!canvasRef.current) {
@@ -99,9 +115,56 @@ export default function Catch() {
     };
   }, [startPaint, paint, exitPaint]);
 
-  return(
-        <div className="App">
-          <canvas ref={canvasRef} height={600} width={800} className="canvas"/>
+  const drawColor = ["black", "red", "blue", "green", "yellow", "purple"]
+  const drawWidth = [1, 3, 5, 7, 9, 11, 13, 15]
+
+  return(<>
+        <div className="canvasContainer">
+          <div className="ButtonContainer">
+            <Button onClick={()=>setIsEraser(!isEraser)}>{isEraser?"붓":'지우개'}</Button>
+            <Button onClick={clearCanvas}>전체 지우기</Button>
+            <div className="colorContainer">
+              붓 색
+              {drawColor.map((color) => <button className="colorButton" style={{backgroundColor: color}} onClick={()=>setColor(color)}></button>)}
+            </div>
+            <div className="lineWidthContainer">
+              {isEraser?"지우개":'붓'} 굵기
+              {drawWidth.map((width) => <button className="lineWidthButton" onClick={()=>setLineWidth(width)}>{width}</button>)}
+            </div>
+          </div>
+          <div className='canvasDiv'>
+            <canvas ref={canvasRef} height={600} width={800} className="canvas"/>
+          </div>
         </div>
+        <style jsx>{`
+        .canvasContainer {
+          height: 100vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+        }
+        .canvasDiv{
+          width: 70vw;
+          height: 75vh;
+          border: 1px solid black;
+          border-radius: 20px;
+          overflow: hidden;
+          background-color: white;
+        }
+        .colorContainer{
+          height: 50px;
+        }
+        .colorButton{
+          width: 20px;
+          height: 20px;
+          border-radius: 50%;
+          margin: 5px;
+        }
+        `}</style>
+        </>
       );
 };
+
+//지우개, 전체 지우기, 색
+// QR 페이지의 하위 컴포넌트로 게임들을 가각 불러오게.
