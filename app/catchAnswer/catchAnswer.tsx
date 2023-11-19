4310b021d3b92c26761b76b8f493e32e6e7b5306
Designated-Hitter <dosadola@gmail.com>
2023-11-19 19:44:19 +0900

    feat: implementing socket io feature

--- app/catchAnswer/catchAnswer.tsx
+++ app/catchAnswer/catchAnswer.tsx
@@ -1,26 +1,30 @@
 import Button from '@mui/material/Button';
 import io from 'socket.io-client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAtom } from 'jotai';
 import { userInfoAtoms } from "@/app/modules/userInfoAtom";
 
 //server와 front가 같은 domain인 경우
 //다른 경우 수정해야 함!
-// const socket = io();
+
+const socket = io();
 
 export default function CatchAnswer() {
     const [userInfo,] = useAtom(userInfoAtoms);
     const [catchAnswer, setCatchAnswer] = useState<string>('');
 
-    // socket.on("connect", () => {
-    //     console.log(socket.id);
-    //     console.log(socket.connected);
-    // });
+    useEffect(() => {
+        socket.on("connect", () => {
+            console.log(socket.id);
+            console.log(socket.connected);
+        });
+    }
+    , []);
 
-    // const handleAnswerSubmit = () => {
-    //     socket.emit("hostId-submit", { nickname: userInfo.nickname });
-    //     socket.emit("answer-submit", { catchAnswer });
-    // }
+    const handleAnswerSubmit = () => {
+        socket.emit("hostId-submit", { nickname: userInfo.nickname });
+        socket.emit("answer-submit", { catchAnswer });
+    }
 
     return (
         <>
@@ -30,7 +34,7 @@ export default function CatchAnswer() {
             className="catchAnswer-input"
             value={catchAnswer}
             onChange={(e)=>setCatchAnswer(e.target.value)}></input>
-        {/* <Button onClick={ handleAnswerSubmit }>제출</Button> */}
+        <Button onClick={ handleAnswerSubmit }>제출</Button>
         </>
     )
 }
