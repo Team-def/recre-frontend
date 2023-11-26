6f4d189dbaa0edaf02af30e5f9e980a2ec5aa9e3
Designated-Hitter <dosadola@gmail.com>
2023-11-26 20:40:02 +0900

    feat: implement skeleton page but we need HTTPS

--- app/playerComponent/flowerPlayer.tsx
+++ app/playerComponent/flowerPlayer.tsx
@@ -1,9 +1,30 @@
+"use client";
 import { useEffect, useState } from 'react';
 
 export default function FlowerPlayer() {
+    //iphone 13 이상의 사파리 브라우저에서는 모션 이벤트 권한을 요청해야 함
+    const requestPermission = (eventType: 'devicemotion' | 'deviceorientation') => {
+        if (window.DeviceOrientationEvent !== undefined && typeof (window.DeviceOrientationEvent as any).requestPermission === 'function') {
+          (window.DeviceOrientationEvent as any).requestPermission()
+            .then(() => {
+              // Permission granted, you might want to do something here
+              window.addEventListener(eventType, () => {});
+            })
+            .catch((error: any) => {
+              console.error(`Error requesting permission: ${error}`);
+            });
+        } else {
+          // Permission not needed or not supported
+          window.addEventListener(eventType, () => {});
+        }
+    };
+    
     const [shakeCount, setShakeCount] = useState(0);
 
     useEffect(() => {
+        requestPermission('devicemotion');
+        requestPermission('deviceorientation');
+
         let x:number = 0;
         let y:number = 0;
         let z:number = 0;
