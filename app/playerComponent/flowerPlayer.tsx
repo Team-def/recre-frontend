c7d0b6cb925b3eca57364e79fa14967020c1fc33
Designated-Hitter <dosadola@gmail.com>
2023-11-27 17:46:06 +0900

    feat: implement flower skeleton

--- app/playerComponent/flowerPlayer.tsx
+++ app/playerComponent/flowerPlayer.tsx
@@ -1,65 +1,50 @@
 "use client";
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function FlowerPlayer() {
-    //iphone 13 이상의 사파리 브라우저에서는 모션 이벤트 권한을 요청해야 함
-    const requestPermission = (eventType: 'devicemotion' | 'deviceorientation') => {
-        if (window.DeviceOrientationEvent !== undefined && typeof (window.DeviceOrientationEvent as any).requestPermission === 'function') {
-          (window.DeviceOrientationEvent as any).requestPermission()
-            .then(() => {
-              // Permission granted, you might want to do something here
-              window.addEventListener(eventType, () => {});
-            })
-            .catch((error: any) => {
-              console.error(`Error requesting permission: ${error}`);
-            });
-        } else {
-          // Permission not needed or not supported
-          window.addEventListener(eventType, () => {});
+    const [shakeCount, setShakeCount] = useState(0);
+    
+    const handleShake = () => {
+        setShakeCount((prevCount) => prevCount + 1);
+    }
+
+    //device의 움직임을 읽어오는 함수
+    const handleDeviceMotion = (event: DeviceMotionEvent) => {
+        const { acceleration } = event;
+
+        if (acceleration) {
+            const { x, y, z } = acceleration;
+            const accelerationMagnitude = Math.sqrt( (x??0) ** 2 + (y??0) ** 2 + (z??0) ** 2);
+            const shakeThreshold = 15;
+            if (accelerationMagnitude > shakeThreshold) {
+                handleShake();
+            }
         }
     };
-    
-    const [shakeCount, setShakeCount] = useState(0);
 
-    useEffect(() => {
-        requestPermission('devicemotion');
-        requestPermission('deviceorientation');
+    //iOS 13 이상의 safari 브라우저에서는 모션 이벤트 권한을 요청해야 함
+    const isSafariOver13 = typeof window.DeviceOrientationEvent.requestPermission === 'function';
 
-        let x:number = 0;
-        let y:number = 0;
-        let z:number = 0;
-        let lastUpdate = 0;
-        
-        const shakeListener = (event: DeviceMotionEvent) => {
-            const { acceleration } = event;
-            
-            if (acceleration) {
-                const curTime = new Date().getTime() / 1000;
-                if ((curTime - lastUpdate) > 1) {
-                    const diffTime = curTime - lastUpdate;
-                    lastUpdate = curTime;
-                    
-                    x = acceleration.x || 0;
-                    y = acceleration.y || 0;
-                    z = acceleration.z || 0;
-            
-                    const speed = Math.abs(x + y + z - 3 * 9.81) / diffTime;
-                    if (speed > 30) {
-                        setShakeCount((prevShakeCount) => prevShakeCount + 1);
-                    }
-                }
-            }
-        };
-        
-        window.addEventListener('devicemotion', shakeListener);
-        
-        return () => {
-            window.removeEventListener('devicemotion', shakeListener);
-        };
-    }, [shakeCount]);
-        
+    const requestPermissionSafari = () => {
+        if (isSafariOver13) {
+            window.DeviceOrientationEvent.requestPermission().then((permissionState) => {
+                if (permissionState === 'denied') {
+                    //safari 브라우저를 종료하고 다시 접속하도록 안내하는 화면 필요
+                  console.log('Permission wasn\'t granted. Allow a retry.');
+                  return;
+                } else if (permissionState === 'granted') {
+                    window.addEventListener('devicemotion', handleDeviceMotion);
+                };
+            })
+                   
+        } else {
+            window.addEventListener('devicemotion', handleDeviceMotion);
+            };
+        }
+
     return (
         <div>
+            <button onClick={requestPermissionSafari}>허가</button>
             <p>Shake Count: {shakeCount};</p>
         </div>
     );
