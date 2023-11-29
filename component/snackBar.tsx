d7b66a190ebb7b6072d80456d51700bccf25984d
Designated-Hitter <dosadola@gmail.com>
2023-11-29 12:06:32 +0900

    fix: fix snackbar css

--- component/snackBar.tsx
+++ component/snackBar.tsx
@@ -23,11 +23,11 @@ function MySnackBar({isAns, ans, nick} : {isAns : boolean, ans : string, nick : 
       },
       style: {
         minWidth: '30vw',
-        maxWidth: '30vw',
         minHeight: '10vh',
         whiteSpace: 'nowrap',
         overflow: 'hidden',
-        fontSize: '40px',
+        textOverflow: 'ellipsis',
+        fontSize: '2vw',
       }
     });
   };
