2977aacfca8861a360736d7a9338852fd703765f
ChoiWheatley <chltmdgus604@naver.com>
2023-12-11 01:37:00 +0900

    devops(vercel): fix CORS with next rewrite

--- next.config.js
+++ next.config.js
@@ -1,18 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
-  };
-  
-  module.exports = nextConfig;
+  async rewrites() {
+    return [
+      {
+        source: "/:path*/",
+        //destination: `https://api.choiwheatley.store/:path*/`,
+        destination: process.env.NEXT_PUBLIC_SOCKET_API + "/:path*/",
+      },
+    ];
+  },
+  trailingSlash: true,
+  reactStrictMode: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
