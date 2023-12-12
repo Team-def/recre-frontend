c8a0ca5b6d06381bff9298b6bbcc3e79d27a3eee
Designated-Hitter <dosadola@gmail.com>
2023-12-12 22:06:39 +0900

    fix: delete unused codes

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,6 @@ import Switch, { SwitchProps } from '@mui/material/Switch';
 import { styled } from '@mui/material/styles';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
 
 
 export default function Home() {
