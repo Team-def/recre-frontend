e06ba5fd5e158eb8d3f26cbb3461eaf042ffca8f
Designated-Hitter <dosadola@gmail.com>
2023-12-13 15:25:12 +0900

    feat: add CSS in button

--- app/catch/page.tsx
+++ app/catch/page.tsx
@@ -300,9 +300,9 @@ export default function Catch({ socket }: { socket: Socket }) {
 
   return (
     <>
-      <div className="BGM-container">
+      <div className="BGM-container" onClick={handleBGM}>
         {isBGMOn? <BackgroundMusic /> : <></>}
-        {isBGMOn? <Button onClick={handleBGM} className='handle-BGM-mute'>🔇</Button> : <Button onClick={handleBGM} className='handle-BGM-unmute'>🔈</Button> }
+        {isBGMOn? <Button className='button-mute'>🔇</Button> : <Button className='button-unmute'>🔈</Button> }
       </div>
       <Particle/>
       <div className="canvasContainer">
@@ -484,15 +484,139 @@ export default function Catch({ socket }: { socket: Socket }) {
         #tsparticles{
           z-index: -5;
         }
-        .handle-BGM-mute{
+        .BGM-container{
           position: absolute;
-          top: 10%;
-          right: 10%;
+          top: 1%;
+          right: 26%;
+          cursor: pointer;
         }
-        .handle-BGM-unmute{
+        .button-mute {
+          appearance: button;
+          background-color: #1899D6;
+          border: solid transparent;
+          border-radius: 16px;
+          border-width: 0 0 4px;
+          box-sizing: border-box;
+          color: #FFFFFF;
+          cursor: pointer;
+          display: inline-block;
+          font-family: din-round,sans-serif;
+          font-size: 15px;
+          font-weight: 700;
+          letter-spacing: .8px;
+          line-height: 20px;
+          margin: 0;
+          outline: none;
+          overflow: visible;
+          padding: 13px 16px;
+          text-align: center;
+          text-transform: uppercase;
+          touch-action: manipulation;
+          transform: translateZ(0);
+          transition: filter .2s;
+          user-select: none;
+          -webkit-user-select: none;
+          vertical-align: middle;
+          white-space: nowrap;
+          width: 100%;
+        }
+
+        .button-mute:after {
+          background-clip: padding-box;
+          background-color: #1CB0F6;
+          border: solid transparent;
+          border-radius: 16px;
+          border-width: 0 0 4px;
+          bottom: -4px;
+          content: "";
+          left: 0;
           position: absolute;
-          top: 10%;
-          right: 10%;
+          right: 0;
+          top: 0;
+          z-index: -1;
+        }
+
+        .button-mute:main,
+        .button-mute:focus {
+          user-select: auto;
+        }
+
+        .button-mute:hover:not(:disabled) {
+          filter: brightness(1.1);
+          -webkit-filter: brightness(1.1);
+        }
+
+        .button-mute:disabled {
+          cursor: auto;
+        }
+
+        .button-mute:active {
+          border-width: 4px 0 0;
+          background: none;
+        }
+        .button-unmute {
+          appearance: button;
+          background-color: #1899D6;
+          border: solid transparent;
+          border-radius: 16px;
+          border-width: 0 0 4px;
+          box-sizing: border-box;
+          color: #FFFFFF;
+          cursor: pointer;
+          display: inline-block;
+          font-family: din-round,sans-serif;
+          font-size: 15px;
+          font-weight: 700;
+          letter-spacing: .8px;
+          line-height: 20px;
+          margin: 0;
+          outline: none;
+          overflow: visible;
+          padding: 13px 16px;
+          text-align: center;
+          text-transform: uppercase;
+          touch-action: manipulation;
+          transform: translateZ(0);
+          transition: filter .2s;
+          user-select: none;
+          -webkit-user-select: none;
+          vertical-align: middle;
+          white-space: nowrap;
+          width: 100%;
+        }
+
+        .button-unmute:after {
+          background-clip: padding-box;
+          background-color: #1CB0F6;
+          border: solid transparent;
+          border-radius: 16px;
+          border-width: 0 0 4px;
+          bottom: -4px;
+          content: "";
+          left: 0;
+          position: absolute;
+          right: 0;
+          top: 0;
+          z-index: -1;
+        }
+
+        .button-unmute:main,
+        .button-unmute:focus {
+          user-select: auto;
+        }
+
+        .button-unmute:hover:not(:disabled) {
+          filter: brightness(1.1);
+          -webkit-filter: brightness(1.1);
+        }
+
+        .button-unmute:disabled {
+          cursor: auto;
+        }
+
+        .button-unmute:active {
+          border-width: 4px 0 0;
+          background: none;
         }
       `}</style>
     </>
