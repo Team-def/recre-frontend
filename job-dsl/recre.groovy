a5cd43cef2e7c72602f20f952abb389329d16a55
recre-docker <recredeploy@gmail.com>
2025-01-21 14:05:31 +0000

    fix : registryCredentialㄴ수정

--- job-dsl/recre.groovy
+++ job-dsl/recre.groovy
@@ -15,11 +15,11 @@ job('Recre Frontend Docker') {
         dockerBuildAndPublish { // 이번엔 shell script 대신에 
             repositoryName('recre-frontend/recre-frontend-docker') // docker hub 레포 이름
             tag('${GIT_REVISION,length=9}') // 태그 - 빈값이면 latest, GIT_REVISION은 고유한 커밋 sha 값
-            registryCredentials('recre') // docker hub id
+            registryCredentials('60b258b5-8387-4fab-b6c0-ccf4252bb0de') // docker hub id
             forcePull(false) // 빌드하기 직전에 최신 이미지 갖고올지
             forceTag(false)
             createFingerprints(false)
             skipDecorate()
         }
     }
-	}
+}
